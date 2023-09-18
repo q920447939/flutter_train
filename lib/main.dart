112454ce549edcac3d9087233979b6cfaf36dac3
frog <you@example.com>
2023-09-18 09:51:54 +0800

    flutter_train

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'navigate/tab_navigate.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '实战携程',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: TabNavigator(),
+    );
+  }
+}
---
+++ lib/navigate/tab_navigate.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_train/pages/home_page.dart';
+import 'package:flutter_train/pages/my_page.dart';
+import 'package:flutter_train/pages/search_page.dart';
+import 'package:flutter_train/pages/travel_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  const TabNavigator({super.key});
+
+  @override
+  State<TabNavigator> createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final PageController _pageController = PageController(initialPage: 0);
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.blueAccent;
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: [
+          HomePage(),
+          SearchPage(),
+          TravelPage(),
+          MyPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _pageController.jumpToPage(index);
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _defaultColor,
+            ),
+            activeIcon: Icon(
+              Icons.home,
+              color: _activeColor,
+            ),
+            label: '首页',
+            tooltip: '首页',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _defaultColor,
+            ),
+            activeIcon: Icon(
+              Icons.search,
+              color: _activeColor,
+            ),
+            label: '搜索',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.camera_alt,
+              color: _defaultColor,
+            ),
+            activeIcon: Icon(
+              Icons.camera_alt,
+              color: _activeColor,
+            ),
+            label: '旅拍',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+              color: _defaultColor,
+            ),
+            activeIcon: Icon(
+              Icons.account_circle,
+              color: _activeColor,
+            ),
+            label: '我的',
+          ),
+        ],
+      ),
+    );
+  }
+}
